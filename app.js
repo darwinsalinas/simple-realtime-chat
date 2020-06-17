107a7d2f7b2714fc4f7518471bdb12f7264349e3
Darwin Salinas <salinash2000@gmail.com>
2020-06-17 08:47:58 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (request, response) => {
+    console.log('index')
     response.sendFile(__dirname + '/index.html')
 })
 
